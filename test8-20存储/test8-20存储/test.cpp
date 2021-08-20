f1aad5a2c9248e3c7acb008874334f4d12d85184
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-08-20 18:23:41 +0800

    2021.08.20

---
+++ test8-20存储/test8-20存储/test.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	float f = 10.0;
+
+	short b = 10;
+	short int c = 10;
+	return 0;
+}
+
+
+void test()
+{
+	printf("hehe\n");
+}
+
+int main()
+{
+	test();
+	return 0;
+}
+
+
+int main()
+{
+	int a = 20;
+	//000000000000000000000000010100
+	//0x00000014
+	int b = -10;
+	//100000000000000000000000001010--Ô­
+	//111111111111111111111111110101--·´
+	//111111111111111111111111110110--²¹
+	//0xFFFFFFF6
+	return 0;
+}
+
+
