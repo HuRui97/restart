22c41ce47461a7305b2319f75e7052932ba4c702
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-08-20 15:41:20 +0800

    2021.08.20

---
+++ test-8-20/test-8-20/test.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+//void my_strcpy(char* dest, char* src)
+//{
+//	while (*src != '\0')
+//	{
+//		*dest = *src;
+//		src++;
+//		dest++;
+//	} 
+//	*dest = *src;
+//}
+
+//void my_strcpy(char* dest, char* src)
+//{
+//	if (dest != NULL && src != NULL)
+//	{
+//		while (*dest++ = *src++)
+//		{
+//			;
+//		}
+//	}	
+//}
+
+//char * my_strcpy(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	assert(dest != NULL);//╤оят
+//	assert(src != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[] = "##########";
+//	char arr2[] = "bit";
+//	printf("%s\n", my_strcpy(arr1, arr2));
+//	return 0;
+//}
