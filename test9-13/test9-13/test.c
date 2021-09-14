4773bf4af1acc4f74db7994124bee9d54de28da7
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-09-14 10:41:23 +0800

    2021.09.13

---
+++ test9-13/test9-13/test.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+//int main()
+//{
+//	//int* p = (int*)malloc(40);
+//	//if (p == NULL)
+//	//{
+//	//	return 0;
+//	//}
+//	//int* p2 = (int*)realloc(p, 80);
+//	//if (p2 != NULL)
+//	//{
+//	//	p = p2;
+// //   }
+//	//int* p = (int*)realloc(NULL, 40);
+//	//realloc也可直接开辟空间
+//	return 0;
+//}
+
+
+
+int main()
+{
+	//int* p = malloc(40);
+	//*p = 10;//有可能对NULL解引用（malloc）开辟失败
+	//int* p = (int*)malloc(40);//10个int
+	//if (p == NULL)
+	//{
+	//	return 0;
+	//}
+	//int i = 0;
+	//for (i = 0; i < 10; i++)//注意越界
+	//{
+	//	*(p + i) = i;
+	//}
+	//free(p);
+	//p = NULL;
+
+
+	return 0;
+}
