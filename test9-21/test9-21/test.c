714b8d2275206f5e7dd93dae6c5a374503d5bd73
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-09-21 10:43:56 +0800

    2021.09.21

---
+++ test9-21/test9-21/test.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+//struct A
+//{
+//	int a;
+//	short b;
+//	int c;
+//	char d;
+//};
+////16
+//struct B
+//{
+//	int a;
+//	short b;
+//	char c;
+//	int d;
+//};
+////12
+
+
+
+
+//#pragma pack(4)
+//
+//int main(int argc,char*argv[])
+//{
+//	struct tagTest1
+//	{
+//		short a;
+//		char d;
+//		long b;
+//		long c;
+//	};
+//	//12
+//	struct tagTest2
+//	{
+//		long b;
+//		short c;
+//		char d;
+//		long a;
+//	};
+//	//12
+//	struct tagTest3
+//	{
+//		short c;
+//		long b;
+//		char d;
+//		long a;
+//	};
+//	//16
+//	struct tagTest1 stT1;
+//	struct tagTest2 stT2;
+//	struct tagTest3 stT3;
+//
+//	printf("%d %d %d", sizeof(stT1), sizeof(stT2), sizeof(stT3));
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		printf("%d ", arr[i]);
+//	}
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("%d ", arr[i]);
+//	//}
+//	return 0;
+//}
+
+
+
