182e73bed0339848c21387b2220d6410fa9632c5
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-09-23 08:29:27 +0800

    2021.09.23

---
+++ test9-23/test9-23/test.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+
+//#define MAX 100
+//
+//int main()
+//{
+//	int a = MAX;
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+
+
+//#define SQURE(X) (X)*(X)
+////ºê
+//int main()
+//{
+//	//int ret = SQURE(5);//5*5
+//	//printf("%d\n", ret);
+//	int ret = SQURE(5 + 1);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//#define DOUBLE(X) ((X)+(X))
+//int main()
+//{
+//	int a = 5;
+//	int ret = 10 * DOUBLE(a);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//void print(int a)
+//{
+//	printf("the value of a is %d\n", a);
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//printf("the value of a is %d\n", a);
+//	print(a);
+//	print(b);
+//	return 0;
+//}
+
+
+//#define PRINT(X) printf("the value of "# X " is %d\n",X)
+//
+//int main()
+//{
+//	//printf("hello world\n");
+//	//printf("hello" "world\n");
+//	int a = 10;
+//	int b = 20;
+//	PRINT(a);
+//	//printf("the value of " "a" "is %d\n",a);
+//	PRINT(b);
+//
+//	return 0;
+//}
+
+
+//#define CAT(X,Y) X##Y
+//
+//int main()
+//{
+//	int class84 = 2019;
+//	//printf("%d\n", class84);
+//	printf("%d\n", CAT(class, 84));
+//	return 0;
+//}
+
+
+
+
+
