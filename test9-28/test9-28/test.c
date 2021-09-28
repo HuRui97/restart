03ce19de54166eb101c61879fbdf0ad923f1a031
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-09-28 11:33:25 +0800

    2021.09.28

---
+++ test9-28/test9-28/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+
+//int main()
+//{
+//	int i = 0;
+//	int max = 0;
+//	int arr[10] = { 0 };
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("输入第%d个数\n", i + 1);
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("%d\n", max);
+//	return 0  ;
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += i;
+//	}
+//	printf("%d\n", sum);
+//}
+
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1900; i <= 2000; i += 4)
+//	{
+//		if (i % 4 == 0 && i % 100 != 0 || i % 100 == 0 && i % 400 == 0)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a, b;
+//	float x, y;
+//	char c1, c2;
+//	scanf("%d %d", &a, &b);
+//	scanf("%f %e", &x, &y);
+//	scanf("%c %c", &c1, &c2);
+//	return 0;
+//}
+
+
+
