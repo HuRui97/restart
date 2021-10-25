5971ac60def333ab42ed5052c8566c4b2207c38b
HuRui97 <87120510+HuRui97@users.noreply.github.com>
2021-10-25 20:24:49 +0800

    2021.10.25

---
+++ practice10-25/practice10-25/practice.c
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<string.h>
+
+
+//int main()
+//{
+//	printf("Practice makes perfect!\n");
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char v = 'v';
+//	printf("%c%4c\n", v, v);
+//	printf("%2c%2c\n", v, v);
+//	printf("%3c\n", v);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("The size of short is %d bytes.\nThe size of int is %d bytes.\nThe size of long is %d bytes.\nThe size of long long is %d bytes.",
+//		sizeof(short),sizeof(int),sizeof(long),sizeof(long long));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%#o %#X", 1234, 1234);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n =0 ;
+//	int a = 0;
+//	scanf("%d", &n);
+//	while (n)
+//	{
+//		a = n % 10;
+//		printf("%d", a);
+//		n = n / 10;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char ch;
+//	while ((ch = getchar())!=EOF)
+//	{
+//		putchar(ch + 32);
+//		printf("\n");
+//		getchar();
+//	}
+//	return 0;
+//}
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    char a;
+//    scanf("%c", &a);
+//    int i, j, k;
+//    for (i = 1; i <= 5; i++)
+//    {
+//        k = i;
+//        while (5 - k)
+//        {
+//            printf(" ");
+//            k++;
+//        }
+//        j = i;
+//        while (j)
+//        {
+//            printf("%c", a);
+//            j--;
+//            while (j > 0)
+//            {
+//                printf(" %c", a);
+//                j--;
+//            }
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
+
+//#include <stdio.h>
+//int main() 
+//{
+//    char x;
+//    scanf("%c", &x);
+//    for (int i = 1; i <= 5; i++) 
+//    {
+//        for (int j = 5 - i; j > 0; j--) 
+//        {
+//            printf(" ");
+//
+//        }
+//        for (int m = 1; m <= i; m++)
+//            printf("%c ", x);
+//        printf("\n");
+//    }
+//}
+//
+//
+//int main()
+//{
+//    int arr[12] = { 73,32,99,97,110,32,100,111,32,105,116,33 };
+//    for (int i = 0; i < 12; i++)
+//    {
+//        printf("%c", arr[i]);
+//    }
+//    return 0;
+//}
+
+
+
+
